82cb0f4e9270b13ab9b1b6ecfe06ae776d727f27
Higesennine <yasuhito.komano@gmail.com>
2024-11-13 13:46:01 -0800

    created informationCard and GoalsCard in profilePage

--- frontend/src/app/(logged-in)/profile/_components/ProfileCard.tsx
+++ frontend/src/app/(logged-in)/profile/_components/ProfileCard.tsx
@@ -13,16 +13,16 @@ import { FaRegUserCircle } from "react-icons/fa"
 
 export default function ProfileCard() {
     return (
-        <div className="mx-5">
-            <Card className="max-w-[600px] mx-auto">
-                <CardHeader className="flex flex-row items-center justify-between">
-                    <CardTitle>Information</CardTitle>
-                    <Button variant='ghost' size='icon'><Edit className="w-4 h-4" /></Button>
-                </CardHeader>
-                <CardContent>
+        <div className="mt-5">
+            <h1 className="text-3xl font-bold">Information</h1>
+            <Card className="relative max-w-[800px] mx-auto px-5">
+                <CardContent className="w-full mt-5">
                     <div className="flex items-center space-x-6 mb-10">
                         <FaRegUserCircle className="text-3xl" />
                         <div className="text-2xl font-bold">Yasuhito Komano</div>
+                        <Button variant='ghost' size='icon' className="absolute right-5">
+                            <Edit className="w-4 h-4" />
+                        </Button>
                     </div>
                     <div className="grid grid-cols-2 gap-4">
                         <div>
--- frontend/src/app/(logged-in)/profile/page.tsx
+++ frontend/src/app/(logged-in)/profile/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import ProfileCard from './_components/ProfileCard'
+import GoalsCard from './_components/GoalsCard'
 
 export default function page() {
   return (
-    <div>
+    <div className='mx-5'>
       <ProfileCard />
+      <GoalsCard />
     </div>
   )
 }
